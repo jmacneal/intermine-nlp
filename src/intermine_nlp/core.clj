be50de7e0b7305d89d0091b3bc980f7a40a772c4
Jake <jake.macneal@gmail.com>
2018-05-18 19:54:06 -0400

    moved some functionality to parse namespace, initial parsing funcitons

--- src/intermine_nlp/core.clj
+++ src/intermine_nlp/core.clj
@@ -1,5 +1,35 @@
 (ns intermine-nlp.core
+  (:require [imcljs.fetch :as fetch]
+            [intermine-nlp.parse :as parse])
   (:gen-class))
+
+;;; Internal Representation
+;;;
+;;; All natural language queries will be parsed into an internal representation.
+;;; This takes the form of nested maps, with partial SQL-like syntax.
+;;; Example:
+;; Genes in drosophila of length 20 ->
+;;; {:organism "drosophila, :select "Genes", :with "length 20"}
+
+;;; As can be seen, parts of this representation ("Genes", "length 20") will need
+;;; to be translated into PathQuery-friendly terms. That is done using matching,
+;;; in a secondary phase.
+
+(defn read-sentence
+  "Read a sentence from a file (for testing)"
+  [path]
+  (-> path slurp read-string :english))
+
+(defn read-pathquery
+  "Read a PathQuery from a file (for testing)"
+  [path]
+  (-> path slurp read-string :pathquery))
+
+(defn ir-to-query
+  "TODO: translate internal representation (ir) to PathQuery (JSON/XML)"
+  [ir]
+  nil)
+
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/intermine_nlp/parse.clj
@@ -0,0 +1,53 @@
+(ns intermine-nlp.parse
+  (:require [clojure.pprint]
+            [clojure.core.match :refer [match]]
+            [opennlp.nlp :as nlp]
+            [opennlp.treebank :as treebank])
+  (:gen-class))
+
+;;; Internal Representation
+;;;
+;;; All natural language queries will be parsed into an internal representation.
+;;; This takes the form of nested maps, with partial SQL-like syntax.
+;;; Example:
+;; Genes in drosophila of length 20 ->
+;;; {:organism "drosophila, :select "Genes", :with "length 20"}
+
+;;; As can be seen, parts of this representation ("Genes", "length 20") will need
+;;; to be translated into PathQuery-friendly terms. That is done using matching,
+;;; in a secondary phase.
+
+
+(def get-sentences (nlp/make-sentence-detector "resources/models/en-sent.bin"))
+(def tokenize (nlp/make-tokenizer "resources/models/en-token.bin"))
+(def pos-tag (nlp/make-pos-tagger "resources/models/en-pos-maxent.bin"))
+(def chunker (treebank/make-treebank-chunker "resources/models/en-chunker.bin"))
+(def parser (treebank/make-treebank-parser "resources/models/en-parser-chunking.bin"))
+
+(defn prepare-nlquery
+  "Raw natural language queries must end in a period"
+  [text]
+  (let [last-char (subs text (dec (count text)))]
+    (if
+        (= last-char ".") text
+        (str text "."))))
+
+(defn nlquery-to-chunk
+  "Use NLP to convert raw text to chunked form"
+  [text]
+  (-> text prepare-nlquery tokenize pos-tag chunker))
+
+(defn nlquery-to-tree
+  "Use NLP to convert raw text to treebank tree form"
+  [text]
+  (-> text prepare-nlquery vector parser first treebank/make-tree))
+
+;; (defn parse-phrase
+;;   "Match against the "
+;;   [])
+
+;; (defn text-to-ir
+;;   "Match chunked text to internal representation (ir)"
+;;   [text]
+;;   (let [parsed-chunks (chunk-sentence text)]
+;;     ()))
