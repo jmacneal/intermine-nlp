1fc680745ae4b14cf27d0c8022105bc1b7f16470
Jake <jake.macneal@gmail.com>
2018-05-18 19:53:12 -0400

    clojure-opennlp added as dep (may change to CoreNLP)

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject intermine-nlp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "intermine-nlp provides translation from natural (English) language to PathQuery queries."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :license {:name "Gnu Lesser General Public License Version 2.1"
+            :url "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.intermine/imcljs "0.5.1"]
+                 [org.clojure/core.async "0.2.395"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [clojure-opennlp "0.4.0"]
+                 ;; [com.zensols.nlp/parse "0.1.4"]
+                 ]
   :main ^:skip-aot intermine-nlp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
