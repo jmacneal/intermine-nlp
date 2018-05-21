2913a51e615232af45b661ba6bafe99db1928532
Jake <jake.macneal@gmail.com>
2018-05-21 19:00:12 -0400

    json_parse.clj now deprecated, easier to use imcljs

--- src/intermine_nlp/json_parse.clj
+++ src/intermine_nlp/json_parse.clj
@@ -2,17 +2,17 @@
   (:require [clojure.data.json :as json]))
 
 (defn read-json-file
-  "Read a json file into a nested map."
+  "DEPRECATED: Read a json file into a nested map."
   [path]
   (let [json-string (-> path java.io.FileInputStream. slurp)]
     (json/read-str json-string :key-fn keyword)))
+
 
 (defn parse-class-fields
   "Parse an intermine db model (as a nested map).
   Returns a map of :class {:attributes {& attrs} :collections {& colls}}."
   [model]
-  (let [classes (-> model :model :classes)
-        class-names (keys classes)]
+  (let [classes (-> model :model :classes)]
     (apply merge
            (for [[k v] classes]
              (hash-map k (select-keys v [:attributes :collections]))))))
