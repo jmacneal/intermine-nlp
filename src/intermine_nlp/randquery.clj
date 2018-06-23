29eefe544a46957f3cbeb64d50471862d3d3940e
Jake <jake.macneal@gmail.com>
2018-06-23 12:27:12 -0500

    replaced possible-value lookup with dumb value generation, completed basic rand-query generation

--- src/intermine_nlp/randquery.clj
+++ src/intermine_nlp/randquery.clj
@@ -2,7 +2,10 @@
   (:require [imcljs.query :as query]
             [imcljs.path :as im-path]
             [clojure.pprint :refer [pprint]]
-            [intermine-nlp.util :as util]))
+            [clojure.string :as string]
+            [intermine-nlp.util :as util]
+            [imcljs.fetch :as im-fetch]
+            [imcljs.path :as path]))
 
 
 
@@ -51,10 +54,12 @@
                  (conj class-kws (->> class-kws
                                       im-path/join-path
                                       (im-path/relationships model)
+                                      (remove #(some #{(first %)} class-kws))
+                                      (into {})
                                       keys
                                       rand-nth))))
            (catch Exception e nil))
-      nil
+      [(rand-nth (-> model :classes keys ))]
       )))
 
 
@@ -89,39 +94,108 @@
   (let [possible-values (util/possible-values service path)]
     (rand-nth possible-values)))
 
-(def string-ops #{"=" "!=" "CONTAINS" "DOES_NOT_CONTAIN" "LIKE" "LOOKUP" "ONE_OF" "EXACT_MATCH"
-                  "MATCH" "DOES_NOT_MATCH" "IS_NULL" })
+
+;;; other ops: "ONE_OF" "DOES_NOT_CONTAIN" "LOOKUP" "EXACT_MATCH"
+;;;  "MATCH" "DOES_NOT_MATCH"    "IS_NULL"
+
+
+(def string-ops ["=" "!=" "<" ">" "<=" ">=" "CONTAINS" "LIKE"])
+(def int-ops ["=" "!=" "<" ">" "<=" ">="])
+(def bool-ops ["=" "!="])
 
 (defn rand-op
   "Returns a random operation of given type and arity (single vs multiple)."
-  [type arity]
-  "="
+  [type]
+  (case type
+    "java.lang.String" (rand-nth string-ops)
+    "java.lang.Integer" (rand-nth int-ops)
+    "int" (rand-nth int-ops)
+    "java.lang.Double" (rand-nth int-ops)
+    "java.lang.Float" (rand-nth int-ops)
+    "java.lang.Boolean" (rand-nth bool-ops)
+    "org.intermine.objectstore.query.ClobAccess" nil
+    ))
 
-  ;; (case type
-  ;;   "java.lang.String"
-
-  ;;   "java.lang.Integer")
-
-  )
+(defn rand-value
+  "Generate a random value of a given type (doesn't necessarily occur in database)."
+  [type]
+  (str (case type
+         "java.lang.String" (apply str (take 10 (repeatedly #(char (+ (rand 26) 65)))))
+         "java.lang.Integer" (rand-int 10000)
+         "int" (rand-int 10000)
+         "java.lang.Double" (rand 100)
+         "java.lang.Float" (rand 100)
+         "java.lang.Boolean" (rand-nth [true false])
+         "org.intermine.objectstore.query.ClobAccess" nil
+         )))
 
 (defn rand-constraint
-  "Selects a random attribute of given depth at the given path."
+  "Selects a random attribute of given depth at the given path,
+  as well as a compatible operation and value. Returns a map containing
+  :path :op :value."
   [service class-kws depth]
   (let [attr (rand-attribute (:model service) class-kws depth)
         path (attr->path attr)
         field-type (-> attr :field :type)]
     {:path path
-     :op (rand-op field-type nil)
-     :value (rand-possible-value service path)}))
+     :op (rand-op field-type)
+     :value (rand-value field-type)}))
 
 (defn rand-depth-constraint
   "Returns a random constraint with depth between 1 and 'depth'.
   Default is flatly distributed, with optional weight vector for
   specifying a probability distribution. class-kws specifies path."
-  ([model class-kws max-depth]
+  ([service class-kws max-depth]
    (let [depth (inc (rand-int max-depth))]
-     (rand-constraint model class-kws depth)))
-  ([model class-kws max-depth probs]
+     (rand-constraint service class-kws depth)))
+  ([service class-kws max-depth probs]
    (let [weighted-vec (flatten (map repeat probs max-depth))
          depth (rand-nth weighted-vec)]
-     (rand-constraint model class-kws depth))))
+     (rand-constraint service class-kws depth))))
+
+(defn rand-view
+  "Selects n random views (paths) of given depth."
+  [model class-kws num-views depth]
+  (let [attrs (repeatedly num-views (partial rand-attribute model class-kws depth))]
+    (vec (map (partial attr->path) attrs))))
+
+(defn rand-summary-view
+  "Like rand-view, but returns a summary view for a relation (class) rather than a
+  field (attribute)."
+  [service class-kws depth]
+  (letfn [(merge-paths [path summaries]
+            (string/join "." (-> path
+                                 (string/split #"\.")
+                                 drop-last
+                                 (concat (string/split summaries #"\."))
+                                 )))]
+    (let [model (:model service)
+          class-kws (:path (rand-attribute model class-kws (dec depth)))
+          path (im-path/join-path class-kws)
+          top-path (im-path/adjust-path-to-last-class model path)
+          top-path-kw (keyword top-path)
+          summary (top-path-kw (util/summaries service))]
+      (vec (map (partial merge-paths path) summary)))))
+
+
+(defn rand-query
+  "Returns a random constraint with depth between 1 and 'depth'.
+  Default is flatly distributed, with optional weight vector for
+  specifying a probability distribution. class-kws specifies path."
+  [service num-views num-summaries num-constraints]
+  (let [model (:model service)
+        view-max-depth 5
+        constraint-max-depth 5
+        view-depth (inc (rand-int view-max-depth))
+        constraint-depth (inc (rand-int constraint-max-depth))
+        class-kw (rand-path model [])]
+    {:select (vec (flatten (concat
+                            (rand-view model class-kw num-views view-depth)
+                            (repeatedly num-summaries (partial rand-summary-view service class-kw view-depth)))))
+     :where (vec (repeatedly num-constraints
+                         (partial rand-depth-constraint service class-kw constraint-depth)))})
+  ;; ([service class-kws max-depth probs]
+  ;;  (let [weighted-vec (flatten (map repeat probs max-depth))
+  ;;        depth (rand-nth weighted-vec)]
+  ;;    (rand-constraint service class-kws depth)))
+  )
