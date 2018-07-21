bffd74235c675c7897ac42ef126745417e7a5dc3
Jake <jake.macneal@gmail.com>
2018-07-21 18:21:28 -0500

    added optional keyword :lemmatize to model-parser for lemmatizing class/attr paths

--- src/intermine_nlp/parse.clj
+++ src/intermine_nlp/parse.clj
@@ -1,26 +1,30 @@
 (ns intermine-nlp.parse
   (:require [clojure.core.match :refer [match]]
             [instaparse.core :as insta]
-            ;; [instaparse.cfg :refer [ebnf]]
             [instaparse.combinators :refer :all]
             [imcljs.path :as im-path]
-            [clojure.pprint :refer [pprint]])
+            [clojure.pprint :refer [pprint]]
+            [intermine-nlp.nlp :as nlp])
   (:gen-class))
 
 (defn model-parser
   "Generate a parser for an intermine model.
-  Consists of 2 productions: one for classes, one for all fields/attributes."
-  [model & [options]]
+  Consists of 2 productions: one for classes, one for all fields/attributes.
+  options:
+         :lematize (default = false)
+  "
+  [model & {:as options}]
   (let [classes (:classes model)
         class-kws (keys classes)
         class-paths (map #(im-path/join-path [%]) class-kws)
         attr-map (map #(hash-map :class % :attrs (im-path/attributes model %)) class-paths)
         attr-keys (distinct (flatten (map #(->> % :attrs keys)  attr-map)))
-        attrs (map name attr-keys)]
+        attrs (if (:lemmatize options)
+                (map (comp nlp/lemmatize name) attr-keys)
+                (map name attr-keys))]
     (merge
      {:CLASS (apply alt (map #(string %) class-paths))}
-     {:FIELD (apply alt (map #(string (im-path/join-path [%])) attrs))})
-    ))
+     {:FIELD (apply alt (map #(string %) attrs))})))
 
 (defn gen-parser
   "Generate an instaparse parser object by merging a top-level parser (default: grammar.bnf)
