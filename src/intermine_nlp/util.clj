6d369e1f34f170d3aaf51c7998d0afcd9eb20547
Jake <jake.macneal@gmail.com>
2018-07-21 16:36:56 -0500

    implemented real possible-values function using clj-http library for GET request

--- src/intermine_nlp/util.clj
+++ src/intermine_nlp/util.clj
@@ -6,7 +6,8 @@
             [imcljs.path :as path]
             [clojure.core.async :refer [<! go]]
             [clojure.string :refer [join split]]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [clojure.string :as str]))
 
 (defn read-edn
   "Read an edn file into a map"
@@ -15,9 +16,9 @@
            (-> file java.io.PushbackReader. edn/read))
        (catch Exception e (printf "Couldn't open file '%s'." path))))
 
-(defn possible-values-
+(defn pseudo-possible-values-
   "imcljs.fetch/possible-values seems to be broken, so here's an implementation
-  using only imcljs.fetch/unique-values. 'path' must point to a field, not a class."
+  using only imcljs.fetch/rows. 'path' must point to a field, not a class."
   [service path]
   (let [split-path (split (path/adjust-path-to-last-class (:model service) path) #"\.")
         query {:from (first split-path)
@@ -26,9 +27,24 @@
                   (catch java.lang.Exception e nil))]
        (->> rows :body :results (map first) distinct)))
 
+(def psuedo-possible-values
+    "imcljs.fetch/possible-values seems to be broken, so here's an implementation
+  using only imcljs.fetch/rows. 'path' must point to a field, not a class."
+  (memoize pseudo-possible-values-))
+
+(defn possible-values-
+  "Fetch possible values using the clj-http library, as the imcljs implementations
+  of possible-values and unique-values are broken"
+  [service path]
+  (let [request-path (str "http://" (:root service) "/service/path/values")
+        request-params {:query-params {"path" path} :accept :json}
+        result (http/get request-path request-params)
+        json-result (http/json-decode (:body result))]
+    (map #(get % "value") (get json-result "results"))))
+
 (def possible-values
-  "imcljs.fetch/possible-values seems to be broken, so here's an implementation
-  using only imcljs.fetch/unique-values. 'path' must point to a field, not a class."
+  "Fetch possible values using the clj-http library, as the imcljs implementations
+  of possible-values and unique-values are broken"
   (memoize possible-values-))
 
 (def summaries
