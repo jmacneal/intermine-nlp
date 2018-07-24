e7f56991c459d6c8f6cf9c12055e7d25b04fc7f5
Jake <jake.macneal@gmail.com>
2018-07-24 03:28:05 -0500

    moved option map support to core/parser-pipeline, added basic transformations to parse tree pipeline

--- src/intermine_nlp/core.clj
+++ src/intermine_nlp/core.clj
@@ -3,7 +3,9 @@
             [intermine-nlp.parse :as parse]
             [intermine-nlp.nlp :as nlp]
             [intermine-nlp.model :as model]
-            [intermine-nlp.util :as util])
+            [intermine-nlp.util :as util]
+            [instaparse.core :as insta]
+            [clojure.string :as string])
   (:gen-class))
 
 
@@ -17,15 +19,46 @@
   [path]
   (-> path slurp read-string :pathquery))
 
-(defn ir-to-query
+(defn parse-tree-to-query
   "TODO: translate internal representation (ir) to PathQuery (JSON/XML)"
-  [ir]
-  nil)
+  [tree])
 
-(defn parse-pipeline
-  [model]
-  (let [parser (parse/gen-parser model)]
-    (comp parser (partial clojure.string/join " ") first nlp/lemmatize)))
+
+(def transform-view
+  {:ORGANISM (fn [text] {:from text})
+   :CLASS (fn [text] {:select text})
+   :FIELD (fn [text] {:select text})})
+
+(def transform-constraint
+  {:CLASS (fn [text] [:from text])
+   :FIELD (fn [text] [:select text])})
+
+(def transform-map
+  {:QUERY (fn [& children] (remove string? children))
+   :VIEW (fn [& children] (insta/transform transform-view children))
+   :CONSTR (fn [& children] [:CONSTR (remove string? children)])
+   :VALUE (fn [text] [:VALUE text])})
+
+
+(defn parser-pipeline
+  "Generate a parser pipeline for a given InterMine model.
+  options:
+         :lemmatize (default = false)"
+  [model & {:as options}]
+  (let [class-lemma-map (->> model
+                             :classes
+                             keys
+                             (map name)
+                             (clojure.string/join " ")
+                             nlp/lemma-map)
+        parser (parse/gen-parser model)
+        parser-lemmatized (parse/gen-parser model class-lemma-map)]
+    #(cond->> %
+        (:lemmatize options) nlp/lemmatize-as-text
+        (:lemmatize options) parser-lemmatized
+        (not (:lemmatize options)) parser
+        (:lemmatize options) (insta/transform transform-map))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
