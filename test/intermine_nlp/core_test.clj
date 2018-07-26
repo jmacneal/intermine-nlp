4d653842fdda8413090c2f6657f7474660ad3b94
Jake <jake.macneal@gmail.com>
2018-07-26 02:33:55 -0500

    implemented unit test for core/parser-pipeline (still needs to run against database of user queries)

--- test/intermine_nlp/core_test.clj
+++ test/intermine_nlp/core_test.clj
@@ -1,7 +1,17 @@
 (ns intermine-nlp.core-test
   (:require [clojure.test :refer :all]
-            [intermine-nlp.core :refer :all]))
+            [intermine-nlp.core :as core]
+            [intermine-nlp.model :as model]
+            [instaparse.core :as insta]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parser-pipeline-test
+  (let [service {:root "www.flymine.org/query"}
+        db-model (model/fetch-model service)
+        pipeline (core/parser-pipeline db-model :lemmatize true)])
+  (testing "Testing parser-pipeline"
+    (are [string] (insta/failure? (pipeline string))
+        "This sentence does not parse."
+        "which Genes doesn't parse."
+        "Gene length for all")
+      (are [string] ((complement insta/failure?) (pipeline string))
+        "show me Gene with primaryIdentifier ovo")))
