0756dbe7d4b43a29e1c5539ff0df2a56664ebb48
Jake <jake.macneal@gmail.com>
2018-07-26 01:18:10 -0500

    improved doc strings, added prepare-nlquery, stripped possessive 's from tokenizer output

--- src/intermine_nlp/nlp.clj
+++ src/intermine_nlp/nlp.clj
@@ -10,7 +10,12 @@
 
 ;;; Apache OpenNLP
 (def get-sentences (nlp/make-sentence-detector "resources/nlp_models/en-sent.bin"))
-(def tokenize (nlp/make-tokenizer "resources/nlp_models/en-token.bin"))
+(def tokenize
+  "Split a string into words and punctuation marks.
+  Example:
+         'This is a sentence.' -> '('this' 'is' 'a' 'sentence')"
+  (comp (partial remove #(= % "'s"))
+          (nlp/make-tokenizer "resources/nlp_models/en-token.bin")))
 (def pos-tag (nlp/make-pos-tagger "resources/nlp_models/en-pos-maxent.bin"))
 (def chunker (treebank/make-treebank-chunker "resources/nlp_models/en-chunker.bin"))
 (def parser (treebank/make-treebank-parser "resources/nlp_models/en-parser-chunking.bin"))
@@ -26,17 +31,30 @@
   [text]
   (-> text prepare-nlquery vector parser first treebank/make-tree))
 
+(defn prepare-nlquery
+  "Raw natural language queries must end in a period for some OpenNLP functions"
+  [text]
+  (let [last-char (subs text (dec (count text)))]
+    (if
+        (or
+         (= last-char ".")
+         (= last-char "?")
+         (= last-char "!")) text
+        (str text "."))))
+
 ;;; Stanford CoreNLP
 (defonce core-nlp (delay (StanfordLemmatizer.)))
 
 
 (defn lemmatize
   "Lemmatize a string, converting words to their canonical/infinitive forms.
-  Example: 'These are many words' -> 'these be many word. Returns a vector or strings"
+  Example: 'These are many words' -> 'these be many word. Returns a vector of strings
+  (one string per word). Removes possessive 's'."
   [^String text]
   (let [^StanfordLemmatizer nlp @core-nlp]
     (->> (.lemmatize nlp text)
          vec
+         (map (partial remove #(= % "'s")))
          (map vec))))
 
 (defn lemmatize-as-text
@@ -50,10 +68,10 @@
 (defn lemma-map
   "Lemmatize a string, returning a word map between the input words and their
   lemmatized form.
-  Example: 'Hello, "
+  Example: 'It's nice meeting you' -> {'It' 'it', 'nice' 'be', 'meeting' 'nice', 'you' 'meeting'}"
   [^String text]
   (let [^StanfordLemmatizer nlp @core-nlp
-        tokens (tokenize text)]
+        dumb-tokens (tokenize text)]
     (->> text
          lemmatize
          flatten
