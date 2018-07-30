ee94e7388089fc23b4ebc8023e73bde5b8d19141
Jake <jake.macneal@gmail.com>
2018-07-30 00:34:48 -0500

    removed commented-out main function from StanfordLemmatizer.java

--- java/StanfordLemmatizer.java
+++ java/StanfordLemmatizer.java
@@ -43,28 +43,4 @@ public class StanfordLemmatizer {
         }
         return lines;
     }
-    // public static void main(String[] args) {
-    //     System.out.println("Starting Stanford Lemmatizer");
-    //     String text = "How could you be seeing into my eyes like open doors? \n"+
-    //             "You led me down into my core where I've became so numb \n"+
-    //             "Without a soul my spirit's sleeping somewhere cold \n"+
-    //             "Until you find it there and led it back home \n"+
-    //             "You woke me up inside \n"+
-    //             "Called my name and saved me from the dark \n"+
-    //             "You have bidden my blood and it ran \n"+
-    //             "Before I would become undone \n"+
-    //             "You saved me from the nothing I've almost become \n"+
-    //             "You were bringing me to life \n"+
-    //             "Now that I knew what I'm without \n"+
-    //             "You can've just left me \n"+
-    //             "You breathed into me and made me real \n"+
-    //             "Frozen inside without your touch \n"+
-    //             "Without your love, darling \n"+
-    //             "Only you are the life among the dead \n"+
-    //             "I've been living a lie, there's nothing inside \n"+
-    //             "You were bringing me to life.";
-    //     StanfordLemmatizer slem = new StanfordLemmatizer();
-    //     System.out.println(slem.lemmatize(text));
-    // }
-
 }
