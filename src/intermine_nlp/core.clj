3f9362e8c6b1df5cda155e872989221474165ba9
Jake <jake.macneal@gmail.com>
2018-08-06 23:37:53 -0500

    removed unnecessary lemmatization option from parser-pipeline (now assumed), began implementation of query gen

--- src/intermine_nlp/core.clj
+++ src/intermine_nlp/core.clj
@@ -5,39 +5,46 @@
             [intermine-nlp.util :as util]
             [instaparse.core :as insta]
             [clojure.string :as string]
-            [clojure.pprint :refer [pprint]])
+            [clojure.pprint :refer [pprint]]
+            [clojure.set :refer [map-invert]])
   (:gen-class))
 
+(defn gen-view
+  "Merge the values in a map of view elements (:CLASS, :FIELD) into an
+  imcljs view map."
+  [view lemma-class-map lemma-field-map]
+  (let [class (get lemma-class-map (:CLASS view))
+        field (get lemma-field-map (:FIELD view))]
+    (if class
+      (str class "." field)
+      field)))
 
-(defn read-sentence
-  "Read a sentence from a file (for testing)"
-  [path]
-  (-> path slurp read-string :english))
+(defn gen-constraint
+  "Merge the values in a map of constraint elements(:CLASS, :FIELD, :VALUE,
+  :COMPARE, :MULTI_COMPARE, :UNARY_OP) into an imcljs constraint map."
+  [constraints lemma-class-map lemma-field-map])
 
-(defn read-pathquery
-  "Read a PathQuery from a file (for testing)"
-  [path]
-  (-> path slurp read-string :pathquery))
+(defn gen-query
+  "Generate a query from a map containing :VIEW :CONSTRS and :ORGANISM keys."
+  [model query-map]
+  (let [lemma-class-map (map-invert (parse/class-lemma-mapping model))
+        lemma-field-map (map-invert (parse/field-lemma-mapping model))
+        views (flatten (map vals (:VIEW query-map)))
+        constraints (flatten (map vals (:CONSTRS query-map)))]
+    {:select (map #(gen-view % lemma-class-map lemma-field-map) views)
+     :where (map #(gen-constraint % lemma-class-map lemma-field-map) constraints)}))
 
 
 (defn parser-pipeline
   "Generate a parser pipeline for a given InterMine model.
   options:
-         :lemmatize (default = false)"
+  "
   [model & {:as options}]
-  (let [class-lemma-map (->> model
-                             :classes
-                             keys
-                             (map name)
-                             (clojure.string/join " ")
-                             nlp/lemma-map)
-        parser (parse/gen-parser model)
-        parser-lemmatized (parse/gen-parser model class-lemma-map)]
-    #(cond->> %
-        (:lemmatize options) nlp/lemmatize-as-text
-        (:lemmatize options) parser-lemmatized
-        (not (:lemmatize options)) parser
-        ;; (:lemmatize options) transform tree
+  (let [parser (parse/gen-parser model)]
+    #(->> %
+          nlp/lemmatize-as-text
+          parser
+          ;; parse/transform-tree
         )))
 
 (defn -main
