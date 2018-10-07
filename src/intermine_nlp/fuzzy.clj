c97e5aa3a35398ad30a85f35ef78f358ef4ee6ed
Jake <jake.macneal@gmail.com>
2018-10-07 15:18:42 -0400

    remoze-fuzzy now respects dot-seperated class paths

--- src/intermine_nlp/fuzzy.clj
+++ src/intermine_nlp/fuzzy.clj
@@ -4,7 +4,9 @@
             [clojure.string :as string]
             [intermine-nlp.util :as util]
             [clojure.math.combinatorics :as comb]
-            )
+
+            [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
 (defn best-match
@@ -20,10 +22,10 @@
 (defn best-match-pred
   "Returns a function which returns best-match word if score is over threshold,
   otherwise returns original string.
-  Example: (filter (best-match-pred ['apple' 'banana'] 0.5) ['applause'])
-         -> 'apple'
-           (filter (best-match-pred ['apple' 'banana'] 0.7) ['applause'])
-         -> 'applause'"
+  Example: (map (best-match-pred ['apple' 'banana'] 0.5) ['applause'])
+         -> ['apple']
+           (map (best-match-pred ['apple' 'banana'] 0.7) ['applause'])
+         -> ['applause']"
   [dictionary threshold]
   #(let [[match score] (best-match % dictionary)]
      (if (>= score threshold)
@@ -32,14 +34,22 @@
 
 (defn replace-fuzzy
   "Replace all words in a string which match with minimum threshold certainty (0.0-1.0)
-  to their matching partner in a list.
+  to their matching partner in a list. Respects period-serperated paths.
   Example: (replace-fuzzy 'These are amazing words' ['amaze' 'wards'] 0.5)
-         -> 'These are amaze wards"
+         -> 'These are amaze wards'
+           (replace-fuzzy 'a class.parth' ['path'] 0.5)
+         -> 'a class.path'"
   [text dictionary threshold]
   (try
-    (let [split-string (string/split text #" ")
-          match-pred (best-match-pred dictionary threshold)]
-      (string/join " " (map match-pred split-string)))
+    (let [match-pred (best-match-pred dictionary threshold)]
+      (as-> text $
+           (string/split $ #" ")
+           (map #(string/split % #"\.") $)
+           (map #(map match-pred %) $)
+           (map #(string/join "." %) $)
+           (string/join " " $)
+           )
+      )
     (catch Exception e nil)))
 
 (defn replace-class-names
